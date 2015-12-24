d22c36bdf1234d37b362aeb9960df1d0293d84cf
59023g <m.edward.pierce@gmail.com>
2015-12-24 11:57:34 -0600

    index: js: express cleanup w todo

--- index.js
+++ index.js
@@ -1,23 +1,22 @@
 var express = require('express'),
-    http = require('http'),
-    redis = require('redis');
+    http = require('http');
+    // redis = require('redis');
 
 var app = express();
 
 // console.log(process.env.REDIS_PORT_6379_TCP_ADDR + ':' + process.env.REDIS_PORT_6379_TCP_PORT);
 
-// APPROACH 2: Using host entries created by Docker in /etc/hosts (RECOMMENDED)
-var client = redis.createClient('6379', 'redis');
+// var client = redis.createClient('6379', 'redis');
+
+app.use("/", express.static(__dirname + "./app/dist/"));
 
 app.get('/', function(req, res, next) {
   client.incr('counter', function(err, counter) {
-    console.log('bo')
+    console.log('bo');
     if(err) return next(err);
     res.send('This page has been viewedd ' + counter + ' times!');
   });
 });
-
-app.
 
 http.createServer(app).listen(process.env.PORT || 8080, function() {
   console.log('Listening on port ' + (process.env.PORT || 8080));
