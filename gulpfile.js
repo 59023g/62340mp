b8aee2d3d85d0348f3a7dca7d959008cf4142cfb
59023g <m.edward.pierce@gmail.com>
2015-12-24 11:57:11 -0600

    gulpfile: js: sourcemaps, NODE_ENV, uglify settings

--- gulpfile.js
+++ gulpfile.js
@@ -7,7 +7,11 @@ var sourcemaps = require('gulp-sourcemaps');
 var del        = require('del');
 var gutil      = require('gulp-util');
 var plumber    = require('gulp-plumber');
+var resolve    = require('resolve');
 
+var production = (process.env.NODE_ENV === 'development');
+
+gulp.task('default', ['clean', 'js:all']);
 
 // https://www.timroes.de/2015/01/06/proper-error-handling-in-gulp-js/
 var gulp_src   = gulp.src;
@@ -20,16 +24,25 @@ gulp.src       = function() {
   );
 };
 
+// note - dist should be build artifact
 gulp.task('clean', function() {
   return del('./app/dist/**/**')
-  });
+});
 
-gulp.task('js', function() {
-  return browserify('./app/index.js', { debug: true }).bundle()
-    .pipe(source('vendor.js'))
+// todo - split vendor and app dependencies into separate bundles
+gulp.task('js:all', function() {
+  // todo - if necc, support for bower dependencies
+  var b = browserify({
+      entries: './app/index.js',
+      debug: !production
+    }
+  );
+  return b.transform("babelify")
+    .bundle()
+    .pipe(source('vendor.min.js'))
     .pipe(buffer())
     .pipe(sourcemaps.init({ loadMaps: true }))
-    .pipe(uglify({ mangle: false, compress: false }))
-    .pipe(sourcemaps.write('/maps'))
+    .pipe(uglify({ mangle: false }))
+    .pipe(sourcemaps.write('./'))
     .pipe(gulp.dest('./app/dist/'));
 });
