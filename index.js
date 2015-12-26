9d27b4d4c6a7fbab0cb0ace1533857c69400d646
59023g <m.edward.pierce@gmail.com>
2015-12-25 22:23:22 -0600

    node: index: GET Yahoo Finance Data,

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 var express = require('express'),
-    http = require('http');
+    http = require('http'),
+    request = require('request'),
+    fs = require('fs'),
+    routes = require('./routes');
     // redis = require('redis');
+    // https://www.npmjs.com/package/request
 
 var app = express();
 
@@ -10,13 +14,20 @@ var app = express();
 
 app.use("/", express.static(__dirname + "./app/dist/"));
 
-app.get('/', function(req, res, next) {
-  client.incr('counter', function(err, counter) {
-    console.log('bo');
-    if(err) return next(err);
-    res.send('This page has been viewedd ' + counter + ' times!');
-  });
-});
+app.get('/', routes.index);
+
+// GET recession data from Yahoo Finance API
+// https://gist.github.com/fincluster/6145995
+request
+  .get('http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.quotes%20where%20symbol%20IN%20(%22YHOO%22,%22AAPL%22)&format=json&env=http://datatables.org/alltables.env')
+  .on('error', function(err) {
+  console.log(err);
+  })
+  .on('response', function(response) {
+    console.log(response.statusCode); // 200
+    console.log(response.headers['content-type']);
+  })
+  .pipe(fs.createWriteStream('data.json'));
 
 http.createServer(app).listen(process.env.PORT || 8080, function() {
   console.log('Listening on port ' + (process.env.PORT || 8080));
