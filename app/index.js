ce6bf3aae4fcd733e1914bb0ae00936138ea36ff
59023g <m.edward.pierce@gmail.com>
2015-12-25 22:22:43 -0600

    app: index, inputForm: client validation

--- app/index.js
+++ app/index.js
@@ -1,1 +1,13 @@
-'use strict';
+// note - vendor deps
+var React = require('react');
+var ReactDOM = require('react-dom');
+var _ = require('lodash');
+
+// note - app deps
+var inputForm = require('./js/inputForm');
+
+var render = function() {
+  ReactDOM.render(React.createElement(inputForm), document.getElementById('content'));
+};
+
+render();
--- app/js/inputForm.js
+++ app/js/inputForm.js
@@ -1,36 +1,53 @@
 var React = require('react');
+var PropTypes = React.PropTypes;
+var _ = require('lodash');
+
 
 module.exports = React.createClass({
-  getInitialState: function() {
-    return { stockHoldings: '' };
+    getInitialState: function() {
+      return { stockHoldings: '', isNumber: false };
+    },
+    handleStockHoldingsChange: function(e) {
+      // todo - check out React.PropTypes to check type
+      // todo - parseInt if lead
+      console.log(Number( e.target.value )  )
+      if ( !Number( e.target.value ) ) {
+        this.setState({ isNumber: false, stockHoldings: '' });
+        return;
+      }
+      this.setState({
+        isNumber: true,
+        stockHoldings: e.target.value
+      });
 
-  },
-  handleStockHoldingsChange: function(e) {
-
-    this.setState({ stockHoldings: e.target.value });
-  },
-  handleSubmit: function(e) {
-    console.log(e);
-    e.preventDefault();
-    var stockHoldings = this.state.stockHoldings.trim();
-    if ( !stockHoldings ) {
-      return;
+    },
+    handleSubmit: function(e) {
+      e.preventDefault();
+      var stockHoldings = this.state.stockHoldings.trim();
+      if (!stockHoldings) {
+        return;
+      }
+      // this.props.onCommentSubmit({ stockHoldings: stockHoldings });
+      this.setState({
+        stockHoldings: stockHoldings
+      });
+    },
+    render: function() {
+      return (
+        < form className = "inputForm" onSubmit = { this.handleSubmit  } >
+          < input
+            type        = "text"
+            placeholder = "Holdings"
+            value       = { this.state.stockHoldings }
+            onChange    = { this.handleStockHoldingsChange }
+          />
+          < input
+            type        = "submit"
+            value       = "Post"
+            disabled    = { !this.state.isNumber }
+          />
+          < span className={ this.state.isNumber ? 'hide' : null }> Must be a number. < /span>
+        < /form>
+      );
     }
-    // this.props.onCommentSubmit({ stockHoldings: stockHoldings });
-    this.setState({ stockHoldings: stockHoldings });
-  },
-  render: function() {
-    return (
-      <form className="inputForm" onSubmit={ this.handleSubmit }>
-        <input
-          type="text"
-          placeholder="Holdings"
-          value={ this.state.stockHoldings }
-          onChange={ this.handleStockHoldingsChange }
-        />
-        <input type="submit" value="Post" />
-        <span> { this.state.stockHoldings } </span>
-      </form>
-    );
-  }
 });
