4ff6ea4bec23b4a02d9f30d967f0e8c39cecefd6
59023g <m.edward.pierce@gmail.com>
2015-12-31 17:30:15 -0600

    react-render: js: server-side markup

---
+++ react-render.js
@@ -0,0 +1,22 @@
+var React = require('react');
+var Box = require('./app/js/box.js'); //this is the box component
+
+var Component = React.createClass({
+    render: function() {
+        return (
+            <html>
+                <head>
+                    <title>
+                        React Server Rendering
+                    </title>
+                </head>
+                <body>
+                    <Box text='testing'/>
+                    <script src="dist/vendor.min.js"></script>
+                </body>
+            </html>
+        );
+    }
+});
+
+module.exports = Component;
