878a79ef65683eb5df985ad11ce1aced6b14ce00
59023g <m.edward.pierce@gmail.com>
2015-12-31 17:28:57 -0600

    app/index: js: server render?

--- app/index.js
+++ app/index.js
@@ -1,13 +1,16 @@
 // note - vendor deps
 var React = require('react');
 var ReactDOM = require('react-dom');
-var _ = require('lodash');
+var Component = require('../react-render.js');
+
+// var _ = require('lodash');
+
 
 // note - app deps
-var inputForm = require('./js/inputForm');
-
-var render = function() {
-  ReactDOM.render(React.createElement(inputForm), document.getElementById('content'));
-};
-
-render();
+// var inputForm = require('./js/inputForm');
+//
+// var render = function() {
+//   ReactDOM.renderToString(React.createElement(inputForm), document.getElementById('content'));
+// };
+//
+// render();
