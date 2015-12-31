f298db491851556b8788617c16314f76d8970cd4
59023g <m.edward.pierce@gmail.com>
2015-12-31 17:29:09 -0600

    app/box: js: server render proto

---
+++ app/js/box.js
@@ -0,0 +1,13 @@
+var React = require('react');
+
+var box = React.createClass({
+    render: function() {
+        return (
+            <div >
+                hello
+            </div>
+        );
+    }
+});
+
+module.exports = box;
