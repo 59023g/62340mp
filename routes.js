c42003024c4921b76f0435b5f24a95a93bdc2285
59023g <m.edward.pierce@gmail.com>
2016-01-02 23:47:32 -0600

    routes: js: ReactDom.renderToString

--- routes.js
+++ routes.js
@@ -1,1 +1,14 @@
+var React   = require('react'),
+  ReactDOM  = require('react-dom/server'),
+  Component = require('./react-render.js');
 
+var ComponentFactory = React.createFactory(Component);
+
+var routes = {
+  index: function(req, res) {
+      var markup = ReactDOM.renderToString(ComponentFactory());
+      res.send(markup);
+  }
+};
+
+module.exports = routes;
