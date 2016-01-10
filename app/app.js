b594bddf34b96d17e130204505289f996c6c0f15
59023g <m.edward.pierce@gmail.com>
2016-01-09 21:12:26 -0600

    app: js: <meta>

--- app/app.js
+++ app/app.js
@@ -6,6 +6,7 @@ var Component = React.createClass({
         return (
             <html>
                 <head>
+                    <meta name="viewport" content="width=device-width,initial-scale=1.0" />
                     <title>
                         App
                     </title>
