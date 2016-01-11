97821e4f88412da1add65a12e93ed43407ec6008
59023g <m.edward.pierce@gmail.com>
2016-01-10 22:52:44 -0600

    app/index: js: require d3Chart

--- app/index.js
+++ app/index.js
@@ -1,7 +1,8 @@
 // 'use strict';
 // note - vendor deps
-var React = require('react');
-var ReactDOM = require('react-dom');
+var React = require('react'),
+    ReactDOM = require('react-dom'),
+    d3Chart = require('./js/d3Chart.js');
 
 // note - app deps
 var inputForm = require('./js/userInput.js');
