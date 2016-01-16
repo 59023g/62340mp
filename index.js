b59fd7048557e53461ef13fb76acacf513d6b80b
59023g <m.edward.pierce@gmail.com>
2016-01-15 23:36:13 -0800

    index: js: rm redis

--- index.js
+++ index.js
@@ -8,20 +8,12 @@ var express     = require('express'),
     d3          = require('d3');
     data        = require('./processData');
 
-    // redis = require('redis');
-    // https://www.npmjs.com/package/request
-
 var app = express();
 app.locals.pretty = true;
-
-// console.log(process.env.REDIS_PORT_6379_TCP_ADDR + ':' + process.env.REDIS_PORT_6379_TCP_PORT);
-
-// var client = redis.createClient('6379', 'redis');
 
 app.use('/', express.static(__dirname + '/app/dist/' ));
 
 app.get('*', routes.index);
-
 
 http.createServer(app).listen(process.env.PORT || 8080, function() {
   console.log('Listening on port ' + (process.env.PORT || 8080));
