42a960b61236425ac4c4c34caedf47755e5c74e5
59023g <m.edward.pierce@gmail.com>
2016-01-19 15:32:43 -0800

    app/index: todo for pre-render d3

--- app/index.js
+++ app/index.js
@@ -1,8 +1,9 @@
 // 'use strict';
 // note - vendor deps
 var React = require('react'),
-    ReactDOM = require('react-dom'),
-    d3Chart = require('./js/d3Chart.js');
+    ReactDOM = require('react-dom');
+    // todo - pre-render chart on server
+    // d3Chart = require('./js/d3Chart.js');
 
 // note - app deps
 var inputForm = require('./js/userInput.js');
