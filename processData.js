23ee8712ccb55ffe66cac2b3e75a2cf3141bb546
59023g <m.edward.pierce@gmail.com>
2016-01-19 15:09:40 -0800

    processData: js: wrap request in promise

--- processData.js
+++ processData.js
@@ -1,34 +1,40 @@
-var _       = require('lodash'),
-    request = require('request'),
-    fs      = require('fs'),
-    q       = require('q');
+var _         = require('lodash'),
+  request     = require('request'),
+  fs          = require('fs'),
+  q           = require('q');
 
 var startDate = '2007-01-14',
-    endDate = '2015-11-01',
-    url = 'https://www.quandl.com/api/v3/datasets/YAHOO/INDEX_DJI.json?collapse=monthly&start_date='+ startDate +'&end_date=' + endDate,
-    djiaStore = 'djia_start_date='+ startDate +'&end_date=' + endDate + '.json';
+  endDate     = '2015-11-01',
+  url         = 'https://www.quandl.com/api/v3/datasets/YAHOO/INDEX_DJI.json?collapse=monthly&start_date=' + startDate + '&end_date=' + endDate,
+  djiaStore   = 'djia_start_date=' + startDate + '&end_date=' + endDate + '.json';
 
 
 var data = {
-  processData: function(rawData) {
-    console.log('hi')
-    // var tmp = rawData.dataset.data;
+  processData: function(dataFile) {
+    // todo - async issue here when GET, but not if existing
+    console.log(JSON.parse(fs.readFileSync(dataFile, 'utf8')))
   },
   get: function() {
     var deferred = q.defer();
     fs.stat(djiaStore, function(err, stat) {
       if (err === null) {
-        deferred.resolve('exists: ' + djiaStore)
+        deferred.resolve(djiaStore);
       } else {
-        request
-          .get(url)
+        request({
+            method: 'GET',
+            uri: url
+          })
           .on('error', function(err) {
-            deferred.reject(new Error(err))
+            deferred.reject(err)
           })
           .on('response', function(response) {
-            deferred.resolve(response.statusCode);
+            console.log('response')
+            response.pipe(fs.createWriteStream(djiaStore))
           })
-          .pipe(fs.createWriteStream(djiaStore));
+          .on('end', function(response) {
+            console.log('end')
+            deferred.resolve(djiaStore)
+          })
       }
 
       // https://github.com/Olical/react-faux-dom
@@ -37,11 +43,10 @@ var data = {
     return deferred.promise;
 
   }
-
-
-
 };
 
-data.get().then(data.processData());
+data.get().then(function(res) {
+  data.processData(res);
+});
 
 module.exports = data;
