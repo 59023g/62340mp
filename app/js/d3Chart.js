a30f12c6ae92392b80324f11b6194f18a472bc37
59023g <m.edward.pierce@gmail.com>
2016-01-22 20:04:27 -0800

    d3Chart: js: require()

--- app/js/d3Chart.js
+++ app/js/d3Chart.js
@@ -1,9 +1,4 @@
 var d3 = require('d3'),
-    fs = require('fs'),
-    _ = require('lodash');
-    // todo - pre-render chart on server :/
-    // rawData = require('../../processData.js'),
-    // ReactFauxDom = require('react-faux-dom');
   fs   = require('fs'),
   _    = require('lodash');
 // todo - pre-render chart on server :/
