337d190c0f34850d36d033b118f9bd4ed1bd1745
59023g <m.edward.pierce@gmail.com>
2016-01-22 20:04:03 -0800

    d3Chart: js: GET /data

--- app/js/d3Chart.js
+++ app/js/d3Chart.js
@@ -4,15 +4,27 @@ var d3 = require('d3'),
     // todo - pre-render chart on server :/
     // rawData = require('../../processData.js'),
     // ReactFauxDom = require('react-faux-dom');
+  fs   = require('fs'),
+  _    = require('lodash');
+// todo - pre-render chart on server :/
+// rawData = require('../../processData.js'),
+// ReactFauxDom = require('react-faux-dom');
 
 var d3Chart = {
-  create: function( el, props, state ) {
+  data: {},
+  get: function(url) {
+    var data = this.data;
+    d3.json(url, function(err, json) {
+      if (err) { return console.warn(err); }
+      data = json;
+    });
 
   }
 
 };
 
 
+d3Chart.get("/data");
 
 
 module.exports = d3Chart;
