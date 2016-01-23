6c765ac16a0860fc72247748c2bbc90accd732fb
59023g <m.edward.pierce@gmail.com>
2016-01-22 20:04:39 -0800

    app/index: include d3Chart for client

--- app/index.js
+++ app/index.js
@@ -1,9 +1,9 @@
 // 'use strict';
 // note - vendor deps
 var React = require('react'),
-    ReactDOM = require('react-dom');
+    ReactDOM = require('react-dom'),
     // todo - pre-render chart on server
-    // d3Chart = require('./js/d3Chart.js');
+    d3Chart = require('./js/d3Chart.js');
 
 // note - app deps
 var inputForm = require('./js/userInput.js');
