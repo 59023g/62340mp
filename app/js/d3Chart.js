eead77d13ed4a60c20c7f15ef21ce85811b879b2
59023g <m.edward.pierce@gmail.com>
2016-01-24 21:42:50 -0800

    app/d3Chart: tfw you take a step back

--- app/js/d3Chart.js
+++ app/js/d3Chart.js
@@ -10,30 +10,29 @@ var d3 = require('d3'),
 var d3Chart = {
   data: {},
   get: function(url) {
-
-
-      var assign = function(context) {
-        var cb = function(err, data) {
-          var args = Array.prototype.slice.call(arguments);
-          console.log(args);
-          if (err) {
-            return console.warn(err);
-          }
-          context.data = data.dataset;
-        };
-        return cb;
-      };
-
-
-      d3.json(url, assign(this));
+    return new Promise(function(resolve, reject) {
+      d3.json(url)
+        .on("progress", function() {
+          console.log("progress", d3.event.loaded);
+        })
+        .on("load", function(json) {
+          resolve(json);
+        })
+        .on("error", function(error) {
+          reject(error);
+        })
+        .get();
+    });
 
 
   }
 };
 
 
-d3Chart.get("/data");
-// .then(function(result) {console.log(result); })
+d3Chart.get("/data").then(function(result) {
+  d3Chart.data = result.dataset;
+  console.log(d3Chart.data);
+});
 
 
 module.exports = d3Chart;
