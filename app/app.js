5fa533a05965c82fd011b639fba264abbcab0327
59023g <m.edward.pierce@gmail.com>
2016-01-24 23:16:22 -0800

    app: stylesheet init

--- app/app.js
+++ app/app.js
@@ -6,6 +6,7 @@ var Component = React.createClass({
         return (
             <html>
                 <head>
+                <link rel="stylesheet" href="css.css"/>
                     <meta name="viewport" content="width=device-width,initial-scale=1.0" />
                     <title>
                         App
@@ -14,7 +15,9 @@ var Component = React.createClass({
                 <body>
                   <div id='app'>
                     <UserInput text='testing'/>
+
                   </div>
+                  <div id='chart'></div>
                   <script src="vendor.min.js"></script>
                 </body>
             </html>
