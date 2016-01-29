48cdf1880a412ab86c19398124a67e2fd271ce9a
59023g <m.edward.pierce@gmail.com>
2016-01-29 15:32:35 -0800

    app/d3Chart: js: implement definitive-module-pattern

--- app/js/d3Chart.js
+++ app/js/d3Chart.js
@@ -1,118 +1,126 @@
-var d3 = require('d3'),
-  fs = require('fs'),
-  _ = require('lodash'),
-  q = require('q');
+var d3 = require('d3');
+
 // todo - pre-render chart on server :/
 // rawData = require('../../processData.js'),
 // ReactFauxDom = require('react-faux-dom');
-var d3Chart = {
-  userDataInit: 10000,
-  userData: [],
-  get: function(url) {
-    return new Promise(function(resolve, reject) {
-      d3.json(url)
-        .on("progress", function() {
-          console.log("progress", d3.event.loaded);
+
+var d3Chart = (function() {
+
+  var _private = {
+    formatDate: d3.time.format("%Y-%m-%d"),
+    parseDate: d3.time.format("%Y-%m-%d").parse,
+    width: parseInt(d3.select('#chart').style('width'), 10),
+    height: 1060,
+    userData: []
+  };
+
+  var _public = {
+    userDataInit: 10000,
+    get: function(url) {
+      return new Promise(function(resolve, reject) {
+        d3.json(url)
+          .on("progress", function() {
+            console.log("progress", d3.event.loaded);
+          })
+          .on("load", function(json) {
+            resolve(json);
+          })
+          .on("error", function(error) {
+            reject(error);
+          })
+          .get();
+      });
+    },
+    drawLineChart: function(data) {
+      var x = d3.time.scale()
+        .range([0, _private.width]);
+
+      var y = d3.scale.linear()
+        .range([_private.height, 0]);
+
+      var xAxis = d3.svg.axis()
+        .scale(x)
+        .orient("bottom");
+
+      var yAxis = d3.svg.axis()
+        .scale(y)
+        .orient("left");
+
+      var line = d3.svg.line()
+        .x(function(d, i) {
+          return x(d.date);
         })
-        .on("load", function(json) {
-          resolve(json);
+        .y(function(d) {
+          return y(d.close);
+        });
+
+      var userLine = d3.svg.line()
+        .x(function(d, i) {
+          return x(d[0]);
         })
-        .on("error", function(error) {
-          reject(error);
-        })
-        .get();
-    });
-  },
-  formatDate: d3.time.format("%Y-%m-%d"),
-  parseDate: d3.time.format("%Y-%m-%d").parse,
-  width: parseInt(d3.select('#chart').style('width'), 10),
-  height: 1060,
-  drawLineChart: function(data) {
+        .y(function(d) {
+          return y(d[2]);
+        });
 
-    var x = d3.time.scale()
-      .range([0, d3Chart.width]);
+      var svg = d3.select("div#chart")
+        .append("div")
+        .classed("svg-container", true)
+        .append("svg")
+        .attr("preserveAspectRatio", "xMinYMin meet")
+        .attr("viewBox", "0 0 " + _private.width + " " + _private.height)
+        .classed("svg-content-responsive", true)
+        .append("g");
 
-    var y = d3.scale.linear()
-      .range([d3Chart.height, 0]);
+      data = data.dataset.data;
+      data.reverse();
 
-    var xAxis = d3.svg.axis()
-      .scale(x)
-      .orient("bottom");
+      data.forEach(function(d, i, a) {
 
-    var yAxis = d3.svg.axis()
-      .scale(y)
-      .orient("left");
+        d.date = _private.parseDate(d[0]);
+        d.close = +d[4];
+        var prev = a[i - 1];
 
-    var line = d3.svg.line()
-      .x(function(d, i) {
-        return x(d.date);
-      })
-      .y(function(d) {
-        return y(d.close);
+        if (!prev) {
+          d.delta = 0;
+          d.userClose = _public.userDataInit;
+          _private.userData.push([d.date, d.delta, d.userClose]);
+        } else {
+          d.delta = (d[4] - prev[4]) / prev[4];
+          d.userClose = +(prev.userClose + (prev.userClose * d.delta));
+          _private.userData.push([d.date, d.delta, d.userClose]);
+        }
       });
 
-    var userLine = d3.svg.line()
-      .x(function(d, i) {
-        return x(d[0]);
-      })
-      .y(function(d) {
-        return y(d[2]);
-      });
+      x.domain(d3.extent(data, function(d) {
+        return d.date;
+      }));
+      y.domain([0, d3.max(data, function(d) {
+        return d.close;
+      })]);
 
-    var svg = d3.select("div#chart")
-      .append("div")
-      .classed("svg-container", true)
-      .append("svg")
-      .attr("preserveAspectRatio", "xMinYMin meet")
-      .attr("viewBox", "0 0 " + d3Chart.width + " " + d3Chart.height)
-      .classed("svg-content-responsive", true)
-      .append("g");
+      // todo - numbers not responsive size
+      svg.append("g")
+        .attr("class", "x axis")
+        .attr("dx", ".71em")
+        .call(xAxis);
 
-    data = data.dataset.data;
-    data.reverse();
+      svg.append("path")
+        .datum(data)
+        .attr("class", "line")
+        .attr("d", line);
 
-    data.forEach(function(d, i, a) {
+      svg.append("path")
+        .datum(_private.userData)
+        .attr("class", "user-line")
+        .attr("d", userLine);
+    }
 
-      d.date = d3Chart.parseDate(d[0]);
-      d.close = +d[4];
-      var prev = a[i - 1];
+  };
 
-      if (!prev) {
-        d.delta = 0;
-        d.userClose = d3Chart.userDataInit;
-        d3Chart.userData.push([d.date, d.delta, d.userClose]);
-      } else {
-        d.delta = (d[4] - prev[4]) / prev[4];
-        d.userClose = +(prev.userClose + (prev.userClose * d.delta));
-        d3Chart.userData.push([d.date, d.delta, d.userClose]);
-      }
-    });
+  return _public;
 
-    x.domain(d3.extent(data, function(d) {
-      return d.date;
-    }));
-    y.domain([0, d3.max(data, function(d) {
-      return d.close;
-    })]);
 
-    // todo - numbers not responsive size
-    svg.append("g")
-      .attr("class", "x axis")
-      .attr("dx", ".71em")
-      .call(xAxis);
-
-    svg.append("path")
-      .datum(data)
-      .attr("class", "line")
-      .attr("d", line);
-
-    svg.append("path")
-      .datum(d3Chart.userData)
-      .attr("class", "user-line")
-      .attr("d", userLine);
-  }
-
-};
+})();
 
 // todo - separate draw axis, etc from the line(s)
 d3Chart.get("/data")
