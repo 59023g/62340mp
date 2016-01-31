b12a52dafcd6a9efb5b39c3d4dc81e8cc88eea6b
59023g <m.edward.pierce@gmail.com>
2016-01-30 22:30:06 -0800

    routes: DMP

--- routes.js
+++ routes.js
@@ -1,14 +1,17 @@
-var React   = require('react'),
-  ReactDOM  = require('react-dom/server');
+var React = require('react'),
+  ReactDOM = require('react-dom/server'),
+  App = React.createFactory(require('./app/app.js').mainView);
 
-
-var App = React.createFactory( require('./app/app.js') );
-
-var routes = {
-  index: function(req, res) {
+var routes = (function() {
+  var _public = {
+    index: function(req, res) {
       var markup = ReactDOM.renderToString(App());
       res.send(markup);
-  }
-};
+    }
+  };
+
+  return _public;
+
+})();
 
 module.exports = routes;
