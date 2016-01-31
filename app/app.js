717cf1726327935f7110038bf654f15e66f3d43f
59023g <m.edward.pierce@gmail.com>
2016-01-30 22:29:55 -0800

    app/app: js: DMP

--- app/app.js
+++ app/app.js
@@ -1,28 +1,35 @@
 var React = require('react');
 var UserInput = require('./js/userInput.js'); //this is the box component
 
-var Component = React.createClass({
-    render: function() {
-        return (
-            <html>
-                <head>
-                <link rel="stylesheet" href="css.css"/>
-                    <meta name="viewport" content="width=device-width,initial-scale=1.0" />
-                    <title>
-                        App
-                    </title>
-                </head>
-                <body>
-                  <div id='app'>
-                    <UserInput text='testing'/>
+var App = (function() {
 
-                  </div>
-                  <div id='chart'></div>
-                  <script src="vendor.min.js"></script>
-                </body>
-            </html>
-        );
-    }
-});
+  var _public = {
+    mainView: React.createClass({
+      render: function() {
+          return (
+              <html>
+                  <head>
+                  <link rel="stylesheet" href="css.css"/>
+                      <meta name="viewport" content="width=device-width,initial-scale=1.0" />
+                      <title>
+                          App
+                      </title>
+                  </head>
+                  <body>
+                    <div id='app'>
+                      <UserInput text='testing'/>
 
-module.exports = Component;
+                    </div>
+                    <div id='chart'></div>
+                    <script src="vendor.min.js"></script>
+                  </body>
+              </html>
+          );
+      }
+    })
+  };
+
+  return _public;
+})();
+
+module.exports = App;
