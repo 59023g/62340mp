7ca8dc1c47ad2963f317f7b7017fe757ffd39d1e
59023g <m.edward.pierce@gmail.com>
2016-02-02 19:58:51 -0800

    gulpfile: js: rebundle on /app file changes

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,15 @@
-var gulp       = require('gulp');
-var source     = require('vinyl-source-stream');
-var buffer     = require('vinyl-buffer');
-var browserify = require('browserify');
-var uglify     = require('gulp-uglify');
-var sourcemaps = require('gulp-sourcemaps');
-var del        = require('del');
-var gutil      = require('gulp-util');
-var plumber    = require('gulp-plumber');
-var resolve    = require('resolve');
+var gulp = require('gulp'),
+  source = require('vinyl-source-stream'),
+  buffer = require('vinyl-buffer'),
+  browserify = require('browserify'),
+  uglify = require('gulp-uglify'),
+  sourcemaps = require('gulp-sourcemaps'),
+  del = require('del'),
+  gutil = require('gulp-util'),
+  plumber = require('gulp-plumber'),
+  resolve = require('resolve'),
+  watchify = require('watchify'),
+  notify = require('gulp-notify');
 
 var packageJson = require('./package.json');
 var dependencies = Object.keys(packageJson && packageJson.dependencies || {});
@@ -16,44 +18,74 @@ var production = (process.env.NODE_ENV === 'development');
 
 gulp.task('default', ['clean', 'js:app', 'js:libs']);
 
+
+function handleErrors() {
+  var args = Array.prototype.slice.call(arguments);
+  notify.onError({
+    title: 'Compile Error',
+    message: '<%= error.message %>'
+  }).apply(this, args);
+  this.emit('end'); // Keep gulp from hanging on this task
+}
+
 // https://www.timroes.de/2015/01/06/proper-error-handling-in-gulp-js/
-var gulp_src   = gulp.src;
-gulp.src       = function() {
+var gulp_src = gulp.src;
+gulp.src = function() {
   return gulp_src.apply(gulp, arguments)
     .pipe(plumber(function(error) {
       gutil.log(gutil.colors.red('Error (' + error.plugin + '): ' + error.message));
       this.emit('end');
-    })
-  );
+    }));
 };
+
 
 // note - dist should be build artifact
 gulp.task('clean', function() {
-  return del('./app/dist/**/**');
+  del('./app/dist/**/**').then(function(paths) {
+    console.log('Deleted files and folders:\n', paths.join('\n'));
+    return;
+  });
 });
+
+
 
 gulp.task('js:libs', function() {
   return browserify()
     .require(dependencies)
     .bundle()
-    // .on('error', handleErrors)
+    .on('error', handleErrors)
     //.pipe(uglify({ mangle: false }))
     .pipe(source('libs.js'))
     .pipe(gulp.dest('./app/dist/'));
 });
 
 
-// todo - split vendor and app dependencies into separate bundles
 gulp.task('js:app', function() {
-  // todo - if necc, support for bower dependencies
-  return browserify('./app/index.js')
-    .external(dependencies)
-    .transform("babelify")
-    .bundle()
-    .pipe(source('app.min.js'))
-    .pipe(buffer())
-    .pipe(sourcemaps.init({ loadMaps: true }))
-    //.pipe(uglify({ mangle: false }))
-    .pipe(sourcemaps.write('./'))
-    .pipe(gulp.dest('./app/dist/'));
+  var props = {
+    entries: './app/index.js',
+    cache: {},
+    packageCache: {}
+  };
+
+  var bundler =  watchify(browserify(props));
+  bundler.transform("babelify")
+    .external(dependencies);
+
+  function rebundle() {
+    var stream = bundler.bundle();
+    return stream.on('error', handleErrors)
+      .pipe(source('app.min.js'))
+      .pipe(buffer())
+      .pipe(sourcemaps.init({
+        loadMaps: true
+      }))
+      //.pipe(uglify({ mangle: false }))
+      .pipe(sourcemaps.write('./'))
+      .pipe(gulp.dest('./app/dist/'));
+  }
+  bundler.on('update', function() {
+    rebundle();
+    gutil.log('Rebundle...');
+  });
+  return rebundle();
 });
