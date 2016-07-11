7241526fde36cfd8d26b302deb5085536292fe41
59023g <m.edward.pierce@gmail.com>
2016-07-11 15:31:22 -0700

    node_home/home: update

--- node_home/home.js
+++ node_home/home.js
@@ -1,9 +1,9 @@
 module.exports= (
 `
 <head>
-  <title>Michael Pierce - San Francisco</title>
+  <title>Michael Pierce - San Francisco - 6969339</title>
   <meta name="viewport" content="width=device-width, user-scalable=no">
-  <meta name="description" content="Michael Pierce - San Francisco">
+  <meta name="description" content="Michael Pierce - San Francisco - 6969339">
   <style>
     h1,
     h2,
@@ -11,32 +11,38 @@ module.exports= (
       margin: 0;
       padding: 0
    }
-   ul { padding-left: 20px; } 
+   ul { padding-left: 20px; }
   </style>
   <script type="text/javascript">
     var timerStart = Date.now()
   </script>
 </head>
-<h2>Michael Pierce</h2>
 <ul>
-  <li>Frontend, UI, Web Developer</li>
-  <li>Design and business background</li>
-  <li>San Francisco #1032194</li>
-  <li>Current client: <a href="http://nimasensor.com">6SensorLabs</a> (devops, graphics, frontend)</li>
-  <li>App built with docker, nodejs, express, react, and d3: <a href="/fin">Link</a></li>
-  <li>Use <a href="https://activecollab.com">AC</a> for project management and invoicing</li>
-  <li>This site loaded in <span class="noJs">(no JS)</span><span class="loadTime"></span> milliseconds</li>
+  <li>Current client: <a href="http://nimasensor.com">6SensorLabs</a> (devops, graphics, ecommerce, frontend)</li>
+  <li>Currently expanding</li>
   <li><a href="mailto:hi@mep.im">Say hi</a></li>
+</ul>
+<ul>
+  <li>Frontend, UI, Web Developer (JavaScript, CSS)</li>
+  <li>Product design and business background</li>
+  <li>Use <a href="https://activecollab.com">AC</a> for project management and invoicing</li>
+  <li>San Francisco #1032194</li>
+</ul>
+<ul>
+  <li>App built with Unix, Docker, Nodejs, Express, React, and D3: <a href="/fin">Link</a></li>
+  <li>This site rendered in <span class="noJs">(no JS)</span><span class="loadTime"></span> milliseconds</li>
   <li><a href="https://github.com/59023g">Github</a></li>
 
 </ul>
+
+<img style="width: 300px" src="https://www.dropbox.com/s/rozgoi555jgd562/logo_2016_0503.png?raw=1" alt="Michael Pierce - San Francisco - 6969339">
 <script type="text/javascript">
 document.onreadystatechange = function () {
-  if (document.readyState == "complete") {
-    document.querySelector('.noJs').remove();
-    var loadTime = Date.now() - timerStart;
-    var otherLoad = window.performance.timing.domContentLoadedEventEnd - window.performance.timing.navigationStart;
-    document.querySelector('.loadTime').textContent = loadTime
+  switch (document.readyState) {
+    case "complete":
+      document.querySelector('.loadTime').textContent = window.performance.timing.domContentLoadedEventEnd - window.performance.timing.navigationStart;
+      document.querySelector('.noJs').remove();
+      break;
   }
 }
 </script>
