dc460a59cd92a6824c3904e25339768cf710068d
59023g <m.edward.pierce@gmail.com>
2016-07-11 17:36:56 -0700

    node_home/home: target="_blank"

--- node_home/home.js
+++ node_home/home.js
@@ -14,25 +14,22 @@ module.exports= (
    ul { padding-left: 20px; }
    body { margin: 36px 24px; }
   </style>
-  <script type="text/javascript">
-    var timerStart = Date.now()
-  </script>
 </head>
 <ul>
-  <li>Current client: <a href="http://nimasensor.com">6SensorLabs</a> (devops, graphics, ecommerce, frontend)</li>
+  <li>Current client: <a href="http://nimasensor.com" target="_blank">6SensorLabs</a> (devops, graphics, ecommerce, frontend)</li>
   <li>Currently expanding</li>
   <li><a href="mailto:hi@mep.im">Say hi</a></li>
 </ul>
 <ul>
   <li>Frontend, UI, Web Developer (JavaScript, CSS)</li>
   <li>Product design and business background</li>
-  <li>Use <a href="https://activecollab.com">AC</a> for project management and invoicing</li>
+  <li>Use <a href="https://activecollab.com" target="_blank">AC</a> for project management and invoicing</li>
   <li>San Francisco #1032194</li>
 </ul>
 <ul>
-  <li>App built with Unix, Docker, Nodejs, Express, React, and D3: <a href="/fin">Link</a></li>
+  <li>App built with Unix, Docker, Nodejs, Express, React, and D3: <a href="/fin" target="_blank">Link</a></li>
   <li>This site rendered in <span class="noJs">(no JS)</span><span class="loadTime"></span> milliseconds</li>
-  <li><a href="https://github.com/59023g">Github</a></li>
+  <li><a href="https://github.com/59023g" target="_blank">Github</a></li>
 
 </ul>
 
