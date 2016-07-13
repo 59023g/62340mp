37ab14e98b1315c64c1d104d19d34887c039f597
59023g <m.edward.pierce@gmail.com>
2016-07-13 11:38:19 -0700

    home.js

--- node_home/home.js
+++ node_home/home.js
@@ -1,9 +1,17 @@
 module.exports= (
 `
+<!DOCTYPE html>
+<html lang="en-us">
 <head>
   <title>Michael Pierce - San Francisco - 6969339</title>
+  <link rel="canonical" href="http://mep.im/">
+  <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, user-scalable=no">
-  <meta name="description" content="Michael Pierce - San Francisco - 6969339">
+  <meta name="description" content="Frontend, UI, Web Developer (JavaScript, CSS) - San Francisco, California 94110">
+  <meta property="og:locale" content="en_US">
+  <meta property="og:type" content="website">
+  <meta property="og:title" content="Michael Pierce - San Francisco - 6969339">
+  <meta property="og:description" content="JavaScript, UI, frontend, web application developer">
   <link rel="icon" type="image/png" href="./fin/favicon-96x96.png" sizes="96x96">
   <link rel="icon" type="image/png" href="./fin/favicon-16x16.png" sizes="16x16">
   <link rel="icon" type="image/png" href="./fin/favicon-32x32.png" sizes="32x32">
@@ -19,15 +27,15 @@ module.exports= (
   </style>
 </head>
 <ul>
-  <li>Current client: <a href="http://nimasensor.com" target="_blank">6SensorLabs</a> (devops, graphics, ecommerce, frontend)</li>
+  <li>Current client: <a href="http://nimasensor.com" target="_blank">6SensorLabs</a> (devops, graphics, ecommerce, UI)</li>
   <li>Currently expanding</li>
   <li><a href="mailto:hi@mep.im">Say hi</a></li>
 </ul>
 <ul>
-  <li>Frontend, UI, Web Developer (JavaScript, CSS)</li>
-  <li>Product design and business background</li>
-  <li>Use <a href="https://activecollab.com" target="_blank">AC</a> for project management and invoicing</li>
-  <li>San Francisco #1032194</li>
+  <li>JavaScript, UI, frontend, web application developer</li>
+  <li>Design (CSS) and business background</li>
+  <li>Sole Proprietor - San Francisco #1032194</li>
+  <li>6 years experience. UW-Madison graduate</li>
 </ul>
 <ul>
   <li>Example app built with Unix, Docker, Nodejs, Express, React, and D3: <a href="/fin" target="_blank">Link</a></li>
@@ -35,7 +43,9 @@ module.exports= (
   <li><a href="https://github.com/59023g" target="_blank">Github</a></li>
 
 </ul>
-
+<ul>
+<li>Based in San Francisco, America</li>
+</ul>
 <img style="max-width: 300px; width: 100%" src="https://www.dropbox.com/s/rozgoi555jgd562/logo_2016_0503.png?raw=1" alt="Michael Pierce - San Francisco - 6969339">
 
 <script type="text/javascript">
@@ -50,6 +60,6 @@ document.onreadystatechange = function () {
   }
 }
 </script>
-
+</html>
 `
 )
