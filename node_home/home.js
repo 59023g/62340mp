668aedeb7d7a604fe0f2593d2e95d749559d4f0b
59023g <m.edward.pierce@gmail.com>
2016-10-10 19:34:03 -0400

    home.js: copy update

--- node_home/home.js
+++ node_home/home.js
@@ -27,15 +27,15 @@ module.exports= (
   </style>
 </head>
 <ul>
- <li>Current Customer: <a href="http://time.com/4115398/best-inventions-2015/item/the-sensor-that-sniffs-out-gluten/"  target="_blank">6SensorLabs</a> (devops, graphics, ecommerce, UI)</li>
+ <li>Current Customer: <a href="http://time.com/4115398/best-inventions-2015/item/the-sensor-that-sniffs-out-gluten/"  target="_blank">Nima Labs</a> (Devops, Graphics, UI, Express API)</li>
   <li>Currently expanding</li>
   <li><a href="mailto:hi@mep.im">Say hi</a></li>
 </ul>
 <ul>
-  <li>JavaScript, UI, frontend, web application developer</li>
+  <li>JavaScript, UI, frontend, server, web application developer</li>
   <li>Design (CSS) and business background</li>
   <li>Sole Proprietor - San Francisco #1032194</li>
-  <li>6 years experience. UW-Madison graduate</li>
+  <li>6 years experience (Self-taught). UW-Madison graduate</li>
 </ul>
 <ul>
   <li>Example app built with Unix, Docker, Nodejs, Express, React, and D3: <a href="/fin" target="_blank">Link</a></li>
@@ -44,7 +44,7 @@ module.exports= (
 
 </ul>
 <ul>
-<li>Based in San Francisco, America</li>
+<li>Based in San Francisco, America <3</li>
 </ul>
 <img style="max-width: 300px; width: 100%" src="https://www.dropbox.com/s/rozgoi555jgd562/logo_2016_0503.png?raw=1" alt="Michael Pierce - San Francisco - 6969339">
 
