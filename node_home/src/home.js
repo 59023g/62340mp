acdf29e46b357f324ddb96980a60b3af5d8b11d8
mpierce <mpierce@mpierce.local>
2017-02-06 17:42:13 -0800

    node_home -> node_home/src

---
+++ node_home/src/home.js
@@ -0,0 +1,65 @@
+module.exports= (
+`
+<!DOCTYPE html>
+<html lang="en-us">
+<head>
+  <title>Michael Pierce - San Francisco - 6969339</title>
+  <link rel="canonical" href="http://mep.im/">
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, user-scalable=no">
+  <meta name="description" content="Frontend, UI, Web Developer (JavaScript, CSS) - San Francisco, California 94110">
+  <meta property="og:locale" content="en_US">
+  <meta property="og:type" content="website">
+  <meta property="og:title" content="Michael Pierce - San Francisco - 6969339">
+  <meta property="og:description" content="JavaScript, UI, frontend, web application developer">
+  <link rel="icon" type="image/png" href="./fin/favicon-96x96.png" sizes="96x96">
+  <link rel="icon" type="image/png" href="./fin/favicon-16x16.png" sizes="16x16">
+  <link rel="icon" type="image/png" href="./fin/favicon-32x32.png" sizes="32x32">
+  <style>
+    h1,
+    h2,
+    h3 {
+      margin: 0;
+      padding: 0
+   }
+   ul { padding-left: 20px; }
+   body { margin: 36px 24px; }
+  </style>
+</head>
+<ul>
+ <li>Current Customer: <a href="https://nimasensor.com/press/"  target="_blank">Nima Labs</a> (JavaScript)</li>
+  <li>Currently expanding</li>
+  <li><a href="mailto:hi@mep.im">Say hi</a></li>
+</ul>
+<ul>
+  <li>JavaScript, UI, frontend, server, web application developer</li>
+  <li>Design (CSS) and business background</li>
+  <li>Sole Proprietor - San Francisco #1032194</li>
+  <li>6 years experience. UW-Madison graduate</li>
+</ul>
+<ul>
+  <li>Tiny app: <a href="/fin" target="_blank">Link</a></li>
+  <li>This page rendered in <span class="noJs">(no JS)</span><span class="loadTime"></span> milliseconds</li>
+  <li><a href="https://github.com/59023g" target="_blank">Github</a></li>
+
+</ul>
+<ul>
+<li>Based in San Francisco, America <3</li>
+</ul>
+<img style="max-width: 300px; width: 100%" src="https://www.dropbox.com/s/rozgoi555jgd562/logo_2016_0503.png?raw=1" alt="Michael Pierce - San Francisco - 6969339">
+
+<script type="text/javascript">
+document.onreadystatechange = function () {
+  switch (document.readyState) {
+    case "interactive":
+      console.log(window.performance.timing)
+      var time = window.performance.timing.domLoading - window.performance.timing.navigationStart;
+      document.querySelector('.loadTime').textContent = time;
+      document.querySelector('.noJs').remove();
+      break;
+  }
+}
+</script>
+</html>
+`
+)
