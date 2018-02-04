d5b31a664cd6644408d8527c262f17867ea7c446
root <hi@mep.im>
2018-02-04 19:31:59 +0000

    home.js: copy and design

--- node_home/src/home.js
+++ node_home/src/home.js
@@ -25,7 +25,7 @@ module.exports= (
    ul { padding-left: 20px; margin-bottom: 24px }
  
    body {
-       padding: 16px 24px;
+	   padding: 16px 24px;padding-bottom: 84px;
 	       max-width: 448px;
 	       box-sizing: border-box;
 	       background: #212121;
@@ -33,7 +33,7 @@ module.exports= (
 	       border-top-left-radius: 11px;
 	       border-top-right-radius: 11px;
 	       border-top: 24px solid #303030;
-    line-height: 1.3 
+    line-height: 1.4 
    }
    li { margin-bottom: 6px; list-style-type: circle }
    html { 
@@ -48,13 +48,13 @@ module.exports= (
 	
 </style>
 </head>
+	<!-- I'm not a big marketer. Made this site in 10 minutes :) Say hi --> 
 <ul>
-<li> Current: <a href='https://theconstituent.net'>The Constituent</a></li>
-<li> Current: <a href='https://localcoinportfolio.com'>local-coin_portfolio</a> ( Crypto portfolio tracker with no data stored on server )</li>
-<li>Past:  Cisco/AppDynamics - AppD Labs ( Patent pending. Work presented during <a href='https://www.youtube.com/watch?v=dNaUTzewPrk&feature=youtu.be&t=8m59s' target='_blank'>2017 AppD Summit Keynote</a> )</li>	
- <li>Past:  <a href="https://nimasensor.com/press/" target="_blank">Nima Labs, Inc</a> ( JavaScript )  </li>
- <li> Past: ALTX ( Acquired by <a href='https://addepar.com' target='_blank'>Addepar</a> ) </li>
-  <li><a href="mailto:hi@mep.im">Say hi</a></li>
+<li> Current: <a href='https://theconstituent.net' target='_blank'>The Constituent</a></li>
+<li> Current: <a href='https://localcoinportfolio.com/?json=%5B%7B%22id%22:0,%22input_date_then%22:1514937600000,%22input_symbol_pair%22:%22ZRXBTC%22,%22meta_quote_currency%22:%22ZRX%22,%22meta_base_currency%22:%22BTC%22,%22input_quantity%22:20100,%22input_price_crypto_then_share%22:0.00006471,%22meta_price_est_crypto_then_usd%22:15150,%22input_price_usd_then_share%22:0.9803565,%22meta_total_then_entry_crypto%22:1.300671,%22meta_total_then_entry_usd%22:19705.16565,%22input_exchange%22:%22Binance%22%7D,%7B%22id%22:1,%22input_date_then%22:1513036800000,%22input_symbol_pair%22:%22XRPBTC%22,%22meta_quote_currency%22:%22XRP%22,%22meta_base_currency%22:%22BTC%22,%22input_quantity%22:14500,%22input_price_crypto_then_share%22:0.00001771,%22meta_price_est_crypto_then_usd%22:16227.9999999,%22input_price_usd_then_share%22:0.287397879998229,%22meta_total_then_entry_crypto%22:0.256795,%22meta_total_then_entry_usd%22:4167.26925997432,%22input_exchange%22:%22Binance%22%7D,%7B%22id%22:2,%22input_date_then%22:1515108780000,%22input_symbol_pair%22:%22BATBTC%22,%22meta_quote_currency%22:%22BAT%22,%22meta_base_currency%22:%22BTC%22,%22input_quantity%22:34000,%22input_price_crypto_then_share%22:0.00005,%22meta_price_est_crypto_then_usd%22:15167.62803252,%22input_price_usd_then_share%22:0.7583814016260001,%22meta_total_then_entry_crypto%22:1.7000000000000002,%22meta_total_then_entry_usd%22:25784.967655284003,%22input_exchange%22:%22Poloniex%22%7D,%7B%22id%22:3,%22input_date_then%22:1488787200000,%22input_symbol_pair%22:%22BTCUSDT%22,%22meta_quote_currency%22:%22BTC%22,%22meta_base_currency%22:%22USDT%22,%22input_quantity%22:20,%22input_price_crypto_then_share%22:1,%22meta_price_est_crypto_then_usd%22:1274.3162846,%22input_price_usd_then_share%22:1274.3162846,%22meta_total_then_entry_crypto%22:20,%22meta_total_then_entry_usd%22:25486.325692,%22input_exchange%22:%22Gemini%22%7D%5D' target='_blank'>local-coin_portfolio</a> ( Crypto portfolio tracker with no data stored on server )</li>
+<li>Past: 1099: Cisco/AppDynamics - AppD Labs ( JavaScript, D3, Architecture, Tech lead.  Patent pending. Work presented during <a href='https://www.youtube.com/watch?v=dNaUTzewPrk&feature=youtu.be&t=8m59s' target='_blank'>2017 AppD Summit Keynote</a> )</li>	
+ <li>Past: 1099: <a href="https://nimasensor.com/press/" target="_blank">Nima Labs, Inc</a> ( Full-Stack JavaScript. Built critical profit center systems )  </li>
+ <li> Past: W2: ALTX ( Acquired by <a href='https://addepar.com' target='_blank'>Addepar</a> ) </li>
 </ul>
 <ul>
   <li>JavaScript, App Architecture</li>
@@ -63,14 +63,15 @@ module.exports= (
   <li>6 years experience. UW-Madison graduate</li>
 </ul>
 <ul>
-  <li>Tiny app: <a href="/fin" target="_blank">Link</a></li>
+ <!-- <li>Tiny app: <a href="/fin" target="_blank">Link</a></li> -->
   <li>This page rendered in <span class="noJs">(no JS)</span><span class="loadTime"></span> milliseconds</li>
   <li><a href="https://github.com/59023g" target="_blank">Github</a></li>
 
 </ul>
 <ul>
 <li>Based in San Francisco, America <3</li>
-</ul>
+<li><a href="mailto:hi@mep.im">Say hi</a></li>
+	</ul>
 <img style="max-width: 300px; width: 100%" src="https://www.dropbox.com/s/rozgoi555jgd562/logo_2016_0503.png?raw=1" alt="Michael Pierce - San Francisco - 6969339">
 
 <script type="text/javascript">
